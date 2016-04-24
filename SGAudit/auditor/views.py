17af5d1f8d9a16053b77bd1e8fbae85f941eae41
ragnyll <justanotherjakeyg@gmail.com>
2016-04-23 21:22:26 -0500

    added urls and views for the models

--- SGAudit/auditor/views.py
+++ SGAudit/auditor/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from auditor.models import *
+from auditor.serializers import *
 
-# Create your views here.
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+@csrf_exempt
+def team_list(request):
+    """
+    List all code teams, or create a new snippet.
+    """
+    if request.method == 'GET':
+        teams = Team.objects.all()
+        serializer = TeamSerializer(snippets, many=True)
+        return JSONResponse(serializer.data)
+    """
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
+    """
