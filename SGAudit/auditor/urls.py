89677890961892af7d519d6dbf76dd9a7fb27906
ragnyll <justanotherjakeyg@gmail.com>
2016-05-15 00:28:57 -0500

    all views replaced with generic api views. All http methods are defined for every model now

--- SGAudit/auditor/urls.py
+++ SGAudit/auditor/urls.py
@@ -1,19 +1,22 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from auditor import views
 
 urlpatterns = [
-    url(r'^teams/$', views.team_list),
-    url(r'^teams/(?P<pk>[0-9]+)/$', views.team_detail),
+    url(r'^teams/$', views.team_list.as_view()),
+    url(r'^teams/(?P<pk>[0-9]+)/$', views.team_detail.as_view()),
 
-    url(r'^members/$', views.member_list),
-    url(r'^members/(?P<pk>[0-9]+)/$', views.member_detail),
+    url(r'^members/$', views.member_list.as_view()),
+    url(r'^members/(?P<pk>[0-9]+)/$', views.member_detail.as_view()),
 
-    url(r'^aliases/$', views.alias_list),
-    url(r'^aliases/(?P<pk>[0-9]+)/$', views.alias_detail),
+    url(r'^aliases/$', views.alias_list.as_view()),
+    url(r'^aliases/(?P<pk>[0-9]+)/$', views.alias_detail.as_view()),
 
-    url(r'^services/$', views.service_list),
-    url(r'^services/(?P<pk>[0-9]+)/$', views.service_detail),
+    url(r'^services/$', views.service_list.as_view()),
+    url(r'^services/(?P<pk>[0-9]+)/$', views.service_detail.as_view()),
 
-    url(r'^endpoints/$', views.endpoints_list),
-    url(r'^endpoints/(?P<pk>[0-9]+)/$', views.endpoint_detail),
+    url(r'^endpoints/$', views.endpoints_list.as_view()),
+    url(r'^endpoints/(?P<pk>[0-9]+)/$', views.endpoint_detail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- SGAudit/auditor/views.py
+++ SGAudit/auditor/views.py
@@ -1,122 +1,43 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
-from rest_framework import status
 from auditor.models import *
 from auditor.serializers import *
+from rest_framework import generics
 
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
+class team_list(generics.ListCreateAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
 
-@api_view(['GET', 'POST'])
-def team_list(request):
-    if request.method == 'GET':
-        teams = Team.objects.all()
-        serializer = TeamSerializer(teams, many=True)
-        return JSONResponse(serializer.data)
+class team_detail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
 
-    elif request.method == 'POST':
-        serializer = TeamSerializer(data=request.data, many=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class member_list(generics.ListCreateAPIView):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
 
+class member_detail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
 
-@csrf_exempt
-def team_detail(request, pk):
-    # Retrieve a single team
-    try:
-        team = Team.objects.get(id=pk)
-    except Team.DoesNotExist:
-        return HttpResponse(status=404)
+class alias_list(generics.ListCreateAPIView):
+    queryset = Alias.objects.all()
+    serializer_class = AliasSerializer
 
-    if request.method == 'GET':
-        serializer = TeamSerializer(team)
-        return JSONResponse(serializer.data)
+class alias_detail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Alias.objects.all()
+    serializer_class = AliasSerializer
 
-@csrf_exempt
-def member_list(request):
-    if request.method == 'GET':
-        members = Member.objects.all()
-        serializer = MemberSerializer(members, many=True)
-        return JSONResponse(serializer.data)
+class service_list(generics.ListCreateAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
 
-@csrf_exempt
-def member_detail(request, pk):
-    # Retrieve a single member
-    try:
-        member = Member.objects.get(id=pk)
-    except Member.DoesNotExist:
-        return HttpResponse(status=404)
+class service_detail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
 
-    if request.method == 'GET':
-        serializer = MemberSerializer(member)
-        return JSONResponse(serializer.data)
+class endpoints_list(generics.ListCreateAPIView):
+    queryset = Endpoint.objects.all()
+    serializer_class = EndpointSerializer
 
-
-@csrf_exempt
-def alias_list(request):
-    if request.method == 'GET':
-        aliases = Alias.objects.all()
-        serializer = AliasSerializer(aliases, many=True)
-        return JSONResponse(serializer.data)
-
-@csrf_exempt
-def alias_detail(request, pk):
-    # Retrieve a single member
-    try:
-        alias = Alias.objects.get(id=pk)
-    except Alias.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = AliasSerializer(alias)
-        return JSONResponse(serializer.data)
-
-@csrf_exempt
-def service_list(request):
-    if request.method == 'GET':
-        services = Service.objects.all()
-        serializer = ServiceSerializer(services, many=True)
-        return JSONResponse(serializer.data)
-
-@csrf_exempt
-def service_detail(request, pk):
-    # Retrieve a single member
-    try:
-        service = Service.objects.get(id=pk)
-    except Service.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = ServiceSerializer(service)
-        return JSONResponse(serializer.data)
-
-@csrf_exempt
-def endpoints_list(request):
-    if request.method == 'GET':
-        endpoints = Endpoint.objects.all()
-        serializer = EndpointSerializer(endpoints, many=True)
-        return JSONResponse(serializer.data)
-
-@csrf_exempt
-def endpoint_detail(request, pk):
-    # Retrieve a single member
-    try:
-        endpoint = Endpoint.objects.get(id=pk)
-    except Endpoint.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = EndpointSerializer(endpoint)
-        return JSONResponse(serializer.data)
+class endpoint_detail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Endpoint.objects.all()
+    serializer_class = EndpointSerializer
