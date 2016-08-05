cad651ee374a1d2e8c2684b6a1d47806c62e16d7
Jon Simington <jon.simington@mst.edu>
2016-08-04 23:06:03 -0500

    Add some AJAX examples

--- SGAudit/auditor/urls.py
+++ SGAudit/auditor/urls.py
@@ -3,23 +3,24 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from auditor import views
 
 urlpatterns = [
-    url(r'^teams/$', views.team_list.as_view()),
-    url(r'^teams/(?P<pk>[0-9]+)/$', views.team_detail.as_view()),
+        url(r'^teams/$', views.team_list.as_view()),
+        url(r'^teams/(?P<pk>[0-9]+)/$', views.team_detail.as_view()),
 
-    url(r'^members/$', views.member_list.as_view()),
-    url(r'^members/(?P<pk>[0-9]+)/$', views.member_detail.as_view()),
+        url(r'^members/$', views.member_list.as_view()),
+        url(r'^members/(?P<pk>[0-9]+)/$', views.member_detail.as_view()),
 
-    url(r'^aliases/$', views.alias_list.as_view()),
-    url(r'^aliases/(?P<pk>[0-9]+)/$', views.alias_detail.as_view()),
+        url(r'^aliases/$', views.alias_list.as_view()),
+        url(r'^aliases/(?P<pk>[0-9]+)/$', views.alias_detail.as_view()),
 
-    url(r'^services/$', views.service_list.as_view()),
-    url(r'^services/(?P<pk>[0-9]+)/$', views.service_detail.as_view()),
+        url(r'^services/$', views.service_list.as_view()),
+        url(r'^services/(?P<pk>[0-9]+)/$', views.service_detail.as_view()),
 
-    url(r'^endpoints/$', views.endpoints_list.as_view()),
-    url(r'^endpoints/(?P<pk>[0-9]+)/$', views.endpoint_detail.as_view()),
+        url(r'^endpoints/$', views.endpoints_list.as_view()),
+        url(r'^endpoints/(?P<pk>[0-9]+)/$', views.endpoint_detail.as_view()),
 
-    url(r'^meetings/$', views.meetings_list.as_view()),
-    url(r'^meetings/(?P<pk>[0-9]+)/$', views.meeting_detail.as_view()),
+        url(r'^meetings/$', views.meetings_list.as_view()),
+        url(r'^meetings/(?P<pk>[0-9]+)/$', views.meeting_detail.as_view()),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
