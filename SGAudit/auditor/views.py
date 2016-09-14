9fd3955594409944d95d5da2c55420c99fb559f3
ragnyll <justanotherjakeyg@gmail.com>
2016-09-14 17:09:31 -0500

    added authentication to all models

--- SGAudit/auditor/views.py
+++ SGAudit/auditor/views.py
@@ -6,10 +6,12 @@ from oauth2_provider.ext.rest_framework import TokenHasReadWriteScope, TokenHasS
 
 
 class team_list(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
 class team_detail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
@@ -24,33 +26,41 @@ class member_detail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = MemberSerializer
 
 class alias_list(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     queryset = Alias.objects.all()
     serializer_class = AliasSerializer
 
 class alias_detail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     queryset = Alias.objects.all()
     serializer_class = AliasSerializer
 
 class service_list(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
 
 class service_detail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
 
 class endpoints_list(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     queryset = Endpoint.objects.all()
     serializer_class = EndpointSerializer
 
 class endpoint_detail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     queryset = Endpoint.objects.all()
     serializer_class = EndpointSerializer
 
 class meetings_list(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     queryset = Meeting.objects.all()
     serializer_class = MeetingSerializer
 
 class meeting_detail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     queryset = Meeting.objects.all()
     serializer_class = MeetingSerializer
