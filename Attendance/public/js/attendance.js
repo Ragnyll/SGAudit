7582c022597f1210cfb39d173cd3b24a33c4f84f
Jon Simington <jon.simington@mst.edu>
2016-09-16 02:59:10 -0500

    Asynchronously fetch list of members and build table for LoginView

--- Attendance/public/js/attendance.js
+++ Attendance/public/js/attendance.js
@@ -5,6 +5,7 @@
 //
 // Author: Jon Simington
 // Created: Wed Sep 14 17:30:38 2016 (-0500)
+
 var destination = document.querySelector("#content");
 var { Router,
       Route,
@@ -46,11 +47,60 @@ var HomeView = React.createClass({
     }
 });
 
+
 // 'Login' View -- list of all users registered; user selects their name
 var UserLoginView = React.createClass({
+    componentWillMount() {
+        var result = {}
+        fetchFromAPI("/members/").then(function(members) {
+            var table = document.getElementById('users-table');
+
+            // Loop over every member that got returned
+            for (var i = 0; i < members.length; i++) {
+                // need to insert at index + 1, since index 0 is the header
+                var row = table.insertRow(i + 1);
+                var presentCell = row.insertCell(0)
+                var nameCell = row.insertCell(1);
+                var competitorCell = row.insertCell(2);
+                var teamsCell = row.insertCell(3);
+
+
+                presentCell.innerHTML = "<a href='/mark-present/" + members[i]['id'] + "' class='waves-effect waves-light btn-large green'><span class='fa fa-check'><span></a>&nbsp;<a href='/mark-absent/" + members[i]['id'] + "' class='waves-effect waves-light btn-large red'><span class='fa fa-times'><span></a>"
+
+                nameCell.innerHTML = members[i]['name']
+
+                if (members[i]['is_competitor']) {
+                    competitorCell.innerHTML = "<span class='fa fa-check'></span>"
+                }
+                else {
+                    competitorCell.innerHTML = "<span class='fa fa-times'></span>"
+                }
+
+                // List all teams the user is on
+                for (var j = 0; j < members[i]['on_teams'].length; j ++) {
+                    teamsCell.innerHTML += "<span class='chip'>" + members[i]['on_teams'][j] + "</span>"
+                }
+            }
+        })
+    },
     render: function() {
         return (
-            <h1>UserLoginView</h1>
+                <div>
+                  <h3>Find Your Name Below:</h3>
+                  <hr />
+                  <table id="users-table" className="striped">
+                    <thead id="users-thead">
+                      <tr id="users-thead-row">
+                        <th data-field="mark-attendance">Present?</th>
+                        <th data-field="name">Name</th>
+                        <th data-field="competitor">Is Competitor</th>
+                        <th data-field="teams">On Teams</th>
+                      </tr>
+                    </thead>
+                    <tbody id="users-tbody">
+                    </tbody>
+                  </table>
+                </div>
         )
     }
 })
@@ -73,6 +123,7 @@ var UserRegisterView = React.createClass({
     }
 })
 
+// React Router logic
 ReactDOM.render(
     <Router>
       <Route path="/" component={App}>
@@ -84,3 +135,77 @@ ReactDOM.render(
     </Router>,
     destination
 );
+
+// Returns the value of the cookie with name `cname`
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
+
+// Pings /token and either gets the existing auth token, or fetches a new one
+function getAuthToken() {
+    return new Promise(function(resolve, reject) {
+        var token = '';
+
+        // If the cookie is not undefined, 'undefined', it exists, so
+        if (getCookie('AUTH_TOKEN') != 'undefined' &&
+            getCookie('AUTH_TOKEN') != undefined) {
+            console.log("Found AUTH_TOKEN cookie, not getting a new one")
+            token = getCookie('AUTH_TOKEN')
+            console.log("COOKIE: " + token)
+        }
+        // Otherwise, the cookie doesn't exist, so we must fetch a new token
+        else {
+            console.log("Didn't find AUTH_TOKEN cookie, fetching new one")
+            $.ajax({
+                type: "GET",
+                url: "http://localhost:8001/token",
+                dataType: 'json',
+                async: true
+                // got the token back, so update the variable for return
+            }).done(function(data) {
+                token = data
+                console.log("TOKEN:  " + token)
+            });
+        }
+        resolve(token)
+    })
+}
+
+// Fetches from the Auditor API at a specified endpoint, and resolves the results
+function fetchFromAPI(endpoint) {
+    return new Promise(function(resolve, reject) {
+        console.log("Attempting to fetch from " + endpoint)
+
+        getAuthToken().then(function(token) {
+            // fetch from api now using the second cURL
+            // curl -H "Authorization: Bearer <aut_token>" http://localhost:8000/members/ > error.html
+            console.log("Got token...")
+            var result = {}
+            $.ajax({
+                type: "GET",
+                url: 'http://simington.io' + endpoint,
+                dataType: 'json',
+                async: true,
+                beforeSend: function(request) {
+                    request.setRequestHeader("Authorization", 'Bearer ' + token)
+                }
+            }).done(function(data) {
+                console.log("GOT BACK THE FOLLOWING FROM " + endpoint)
+                console.log(data)
+                result = data
+                resolve(result)
+            })
+        });
+    });
+}
--- Attendance/routes/index.js
+++ Attendance/routes/index.js
@@ -5,9 +5,51 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var MuiThemeProvider = require('material-ui/styles/MuiThemeProvider');
 
-/* GET home page. */
+var secret_settings = require('../secret_settings.js')
+var AUDITOR_USERNAME = secret_settings.AUDITOR_USERNAME
+var AUDITOR_PASSWORD = secret_settings.AUDITOR_PASSWORD
+var CLIENT_ID = secret_settings.CLIENT_ID
+var CLIENT_SECRET = secret_settings.CLIENT_SECRET
+var AUDITOR_URL = secret_settings.AUDITOR_URL
+
+var request = require('request');
+
 router.get('/', function(req, res, next) {
     res.render("attendance")
+});
+
+// Requests a new auth token from the auditor server
+router.get('/token/', function(req, res, next) {
+    var token_cookie = req.cookies['AUTH_TOKEN']
+
+    // Token cookie doesn't exist -> fetch new token and save it as cookie
+    if (token_cookie == undefined || token_cookie == 'undefined') {
+        console.log("Auth token not found.  Creating new one.")
+        request({
+            url: AUDITOR_URL + '/o/token/',
+            method: 'POST',
+            auth: {
+                user: CLIENT_ID,
+                pass: CLIENT_SECRET
+            },
+            form: {
+                'grant_type': 'password',
+                'username': AUDITOR_USERNAME,
+                'password': AUDITOR_PASSWORD
+            }
+        }, function(err, result) {
+            // parse out the new token
+            var new_token = JSON.parse(result.body)['access_token']
+
+            // set AUTH_TOKEN cookie, and send it back as JSON
+            res.cookie('AUTH_TOKEN', new_token).json(new_token)
+        })
+    }
+    // else, token exists, send back the existing token
+    else {
+        console.log("Found Auth token")
+        res.json(token_cookie)
+    }
 });
 
 module.exports = router;
